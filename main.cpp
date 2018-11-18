f753203be867ccbe134d9c32d0eff8724056fed0
Guillermo <guillermodelecea@outlook.es>
2018-11-18 12:07:16 +0100

    Primer programilla

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std; // para evitar tener que poner std::cout cada vez que se quiera imprimir
+
+int main(){
+    string numeroSat;
+    cout<< "Introduzca el número de satélites"<< endl;
+    cin >> numeroSat;
+    cout<< "Ha introducido " << numeroSat<< " satelites";
+
+    
+    return 0;
+
+}
